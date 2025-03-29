d6970eaab9c40f399abe90d48f0a68ec799dd771
Bhargavvv412 <bhargavvv412@gmail.com>
2025-03-30 01:45:49 +0530

    setup

--- setup.py
+++ setup.py
@@ -2,22 +2,25 @@ from setuptools import find_packages,setup
 from typing import List
 
 def get_requirements()->List[str]:
+    """
+    Thiss function will return list of requirements
+    
+    """
     requirement_lst:List[str]=[]
     try:
         with open('requirements.txt','r') as file:
             #Read lines from the file
             lines=file.readlines()
+            ## Process each line
             for line in lines:
-                requirement = line.strip()
-                #ignore empty lines and -e.
-                if requirement and requirement !='-e .':
+                requirement=line.strip()
+                ## ignore empty lines and -e .
+                if requirement and requirement!= '-e .':
                     requirement_lst.append(requirement)
     except FileNotFoundError:
-        print("requirement.txt file not found")
+        print("requirements.txt file not found")
 
     return requirement_lst
-
-print(get_requirements())
 
 setup(
     name="NetworkSecurity",
@@ -25,5 +28,5 @@ setup(
     author="Bhargavvv412",
     author_email="bhargavvv412@gmail.com",
     packages=find_packages(),
-    install_requires = get_requirements()
+    install_requires=get_requirements()
 )
