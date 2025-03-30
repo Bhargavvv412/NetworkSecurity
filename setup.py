8df2611cb6947214704c8c4bdf1f276b886c92e9
Bhargavvv412 <bhargavvv412@gmail.com>
2025-03-30 15:57:04 +0530

    Project is done

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+'''
+The setup.py file is an essential part of packaging and 
+distributing Python projects. It is used by setuptools 
+(or distutils in older Python versions) to define the configuration 
+of your project, such as its metadata, dependencies, and more
+'''
+
 from setuptools import find_packages,setup
 from typing import List
 
@@ -25,8 +32,8 @@ def get_requirements()->List[str]:
 setup(
     name="NetworkSecurity",
     version="0.0.1",
-    author="Bhargavvv412",
-    author_email="bhargavvv412@gmail.com",
+    author="Krish Naik",
+    author_email="krishnaik06@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements()
 )
